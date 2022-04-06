945076b3b8369eb728cb73b1bd4b78f12872a6a5
Eliyas Hossain <eliyashossain700@gmail.com>
2022-04-06 15:00:06 +0600

    Firebase Set Up and react version 18 warning Handel

---
+++ src/_steps.js
@@ -0,0 +1,13 @@
+/*
+Step:1 - Create a project on Console.firebase.google.com
+
+Step:2 - Install Firebase
+
+Step:3-  Register Web app in Firebase
+ Step-4: Copy firebase init from firebase Config Setting
+ step-5 Export default app 
+ step-6 import app firebase.init.js into your app .js 
+ Step-7 : import getAuth from firebase/auth and create const auth =getAuth(app)
+ step-8 : Turn on google authentication(firebase> authentication>enable google sign in 
+     )
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxpBJE0w6mTqsFOtZuCGMUkbSjOq-S_8M",
+  authDomain: "simple-firebase-authenti-674e4.firebaseapp.com",
+  projectId: "simple-firebase-authenti-674e4",
+  storageBucket: "simple-firebase-authenti-674e4.appspot.com",
+  messagingSenderId: "463159005549",
+  appId: "1:463159005549:web:941acd523a0e53613bca64",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
